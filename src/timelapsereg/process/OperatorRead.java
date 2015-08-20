7e59bbb3d9331e195c5b9a36e2bc3078c8252e4a
Raghavender Sahdev <raghavendersahdev@gmail.com>
2015-08-20 23:31:48 +0530

    Create OperatorRead.java

---
+++ src/timelapsereg/process/OperatorRead.java
@@ -0,0 +1,54 @@
+package timelapsereg.process;
+
+import java.io.File;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.Opener;
+import timelapsereg.gui.components.ProcessProgressBar;
+
+public class OperatorRead implements Runnable {
+
+	private ProcessProgressBar	progress;
+	private Thread				thread	= null;
+	private Data				data;
+
+	public OperatorRead(ProcessProgressBar progress, Data data) {
+		this.progress = progress;
+		this.data = data;
+		if (thread == null) {
+			thread = new Thread(this);
+			thread.setPriority(Thread.MIN_PRIORITY);
+			thread.start();
+		}
+	}
+
+	@Override
+	public void run() {
+		data.frames.clear();
+		File dir = new File(data.pathSource);
+		if (!dir.isDirectory()) {
+			IJ.error(data.pathSource + "is not a directory");
+			progress.progress("Invalid source directory", 1);
+			return;
+		}
+		File[] files = dir.listFiles();
+		progress.reset("Read all files");
+		Opener opener = new Opener();
+		int n = files.length;
+		int count = 0;
+		for(File file : files) {
+			progress.progress(file.getName(), count*100.0/n);
+			ImagePlus imp = opener.openImage(file.getAbsolutePath());
+			if (imp != null) {
+				Frame frame = new Frame(data.frames.size()+1, data.framerate, file.getAbsolutePath(), file.getName(), imp);
+				data.frames.add(frame);
+			}
+			count++;
+		}
+		progress.progress("" + data.frames.size() + " frames", 100);
+
+		if (data.frames.size() > 0)
+			data.showFramesAsTable();
+		thread=null;
+}}
